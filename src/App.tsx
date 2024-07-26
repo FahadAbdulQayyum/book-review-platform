5e5c8875f921ac94fd9dd1be3db9eb87f2e81018
Fahad <fahad.45843@iqra.edu.pk>
2024-07-26 22:37:02 +0500

    tailwind setup and navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home'
+import Login from './components/login'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <Navbar />
+      {/* <Home /> */}
+      <Login />
     </>
   )
 }
---
+++ src/components/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='bg-black text-white flex justify-between'>
+            <div>L</div>
+            <div>R</div>
+        </div>
+    )
+}
+
+export default Login
